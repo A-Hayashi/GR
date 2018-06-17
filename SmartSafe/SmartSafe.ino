fcd3dc60b87dd32d9a61c018644f98cb497b41dd
dracen <a.hayashi0113@gmail.com>
2018-06-17 21:00:54 +0900

    RFID いったん削除

--- SmartSafe/SmartSafe.ino
+++ SmartSafe/SmartSafe.ino
@@ -10,7 +10,7 @@
 #include <Servo.h> 
 #include "iodefine.h"
 #include "PS_PAD.h"
-#include "MFRC522.h"
+//#include "MFRC522.h"
 Servo myservo;  // create servo object to control a servo 
 
 int potpin = 0;  // analog pin used to connect the potentiometer
@@ -21,15 +21,11 @@ PS_PAD PAD(PS2_SEL);
 
 #define RC522_SDA        8
 #define RC522_RESET      7
-MFRC522 rfid(RC522_SDA, RC522_RESET);
+//MFRC522 rfid(RC522_SDA, RC522_RESET);
 
 
 void setup() 
-{
-  
-  SPI.begin();
-  rfid.PCD_Init();
-  
+{  
   pinMode(5, OUTPUT);
   pinMode(A0, INPUT);
   pinMode(2, OUTPUT);
@@ -80,84 +76,83 @@ void loop()
     digitalWrite(3,LOW);
     MTU2TSTR |= (1<<2);  //カウント動作開始
   }
-  rfid_main();
-  // sets the servo position according to the scaled value 
-  delay(1000);                           // waits for the servo to get there 
+
+  delay(50);                           // waits for the servo to get there 
 } 
 
 
-bool auth_state = false;
-#define AHTH_TH 10
-void rfid_main()
-{
-  const char expected_uid[] = {0x61, 0x20, 0xC4, 0x30};
-  static int auth_cnt = AHTH_TH;
-
-  if (!rfid.PICC_IsNewCardPresent()) {
-    Serial.println(F("New card is not present"));
-    auth_cnt = max(auth_cnt--, 0);
-    goto END;
-  }
-
-  if (!rfid.PICC_ReadCardSerial()) {
-    Serial.println(F("Card cannot be read"));
-    goto END;
-  }
-
-  {
-    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
-        piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-        piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-      Serial.println(F("Your tag is not of type MIFARE Classic."));
-      auth_cnt = max(auth_cnt--, 0);
-      goto END;
-    }
-  }
-
-  Serial.print("Tap card key: ");
-  for (byte i = 0; i < 4; i++) {
-    Serial.print(rfid.uid.uidByte[i], HEX);
-    if (i < 3) {
-      Serial.print(":");
-    } else {
-      Serial.println("");
-    }
-  }
-
-  if (memcmp(expected_uid, rfid.uid.uidByte, sizeof(expected_uid)) == 0) {
-    auth_cnt = AHTH_TH;
-  } else {
-    Serial.println("**Acces denied**");
-    auth_cnt = max(auth_cnt--, 0);
-  }
-
-END:
-  {
-
-    static bool auth_state_old = false;
-    auth_state = false;
-    Serial.println(auth_cnt);
-
-    if (auth_cnt > 0) {
-      auth_state = true;
-    } else {
-      auth_state = false;
-    }
-
-    if (auth_state_old != auth_state) {
-      if (auth_state == true) {
-        Serial.println("false to true");
-      } else {
-        Serial.println("true to false");
-      }
-    }
-    auth_state_old = auth_state;
-  }
-}
-
-
-
-
-
+//bool auth_state = false;
+//#define AHTH_TH 10
+//void rfid_main()
+//{
+//  const char expected_uid[] = {0x61, 0x20, 0xC4, 0x30};
+//  static int auth_cnt = AHTH_TH;
+//
+//  if (!rfid.PICC_IsNewCardPresent()) {
+//    Serial.println(F("New card is not present"));
+//    auth_cnt = max(auth_cnt--, 0);
+//    goto END;
+//  }
+//
+//  if (!rfid.PICC_ReadCardSerial()) {
+//    Serial.println(F("Card cannot be read"));
+//    goto END;
+//  }
+//
+//  {
+//    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+//    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+//        piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+//        piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+//      Serial.println(F("Your tag is not of type MIFARE Classic."));
+//      auth_cnt = max(auth_cnt--, 0);
+//      goto END;
+//    }
+//  }
+//
+//  Serial.print("Tap card key: ");
+//  for (byte i = 0; i < 4; i++) {
+//    Serial.print(rfid.uid.uidByte[i], HEX);
+//    if (i < 3) {
+//      Serial.print(":");
+//    } else {
+//      Serial.println("");
+//    }
+//  }
+//
+//  if (memcmp(expected_uid, rfid.uid.uidByte, sizeof(expected_uid)) == 0) {
+//    auth_cnt = AHTH_TH;
+//  } else {
+//    Serial.println("**Acces denied**");
+//    auth_cnt = max(auth_cnt--, 0);
+//  }
+//
+//END:
+//  {
+//
+//    static bool auth_state_old = false;
+//    auth_state = false;
+//    Serial.println(auth_cnt);
+//
+//    if (auth_cnt > 0) {
+//      auth_state = true;
+//    } else {
+//      auth_state = false;
+//    }
+//
+//    if (auth_state_old != auth_state) {
+//      if (auth_state == true) {
+//        Serial.println("false to true");
+//      } else {
+//        Serial.println("true to false");
+//      }
+//    }
+//    auth_state_old = auth_state;
+//  }
+//}
+//
+//
+//
+//
+//
 
